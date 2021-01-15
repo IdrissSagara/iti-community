30210d7aa18b84b8d35649c150a2f10576aaf506
Sagara Idriss <sagara.idriss@gmail.com>
2021-01-15 18:54:57 +0100

    [F] error validation  ok

--- src/modules/user/components/user-registration/user-registration.component.ts
+++ src/modules/user/components/user-registration/user-registration.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { UserService } from '../../services/user.service';
+import {UserService} from '../../services/user.service';
+import {UserQueries} from '../../services/user.queries';
 
 class UserRegistrationFormModel {
   username = "";
@@ -15,24 +16,33 @@ class UserRegistrationFormModel {
   styleUrls: ['./user-registration.component.less']
 })
 export class UserRegistrationComponent implements OnInit {
-  @ViewChild("f")
+  @ViewChild('f')
   form: NgForm;
 
   model = new UserRegistrationFormModel();
 
+  checkUserExiste = false;
+  passwordIsNotEquals = false;
+
   constructor(
     private router: Router,
-    private userService: UserService
-  ) { }
+    private userService: UserService,
+    private user: UserQueries,
+  ) {
+  }
 
   ngOnInit(): void {
   }
 
   async submit() {
+    if (this.user.exists(this.model.username)) {
+      this.checkUserExiste = true;
+      return;
+    }
 
     // TODO  Vérifier que la confirmation de mot de passe correspond au mot de passe
     if (this.form.form.invalid || this.model.password !== this.model.confirmPassword) {
-      console.log('Les 2 mot de passe ne concorde pas');
+      this.passwordIsNotEquals = true;
       return;
     } else {
       this.userService.register(this.model.username, this.model.password);
