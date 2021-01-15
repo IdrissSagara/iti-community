be7748f7f66b88927a9958ecbcd1d6e6e7dc0618
vladnelson <didimilca@hotmail.fr>
2021-01-15 20:26:47 +0100

    [Sign Up] - Finalisation de l'inscription avec les messages d'erreur.

--- src/modules/user/components/user-registration/user-registration.component.ts
+++ src/modules/user/components/user-registration/user-registration.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import {UserService} from '../../services/user.service';
-import {UserQueries} from '../../services/user.queries';
+import { UserService } from '../../services/user.service';
+import { UserQueries } from '../../services/user.queries';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 class UserRegistrationFormModel {
   username = "";
@@ -21,35 +22,61 @@ export class UserRegistrationComponent implements OnInit {
 
   model = new UserRegistrationFormModel();
 
-  checkUserExiste = false;
-  passwordIsNotEquals = false;
-
   constructor(
     private router: Router,
     private userService: UserService,
+    private nzMessageService: NzMessageService,
     private user: UserQueries,
   ) {
   }
 
   ngOnInit(): void {
+
   }
 
   async submit() {
-    if (this.user.exists(this.model.username)) {
-      this.checkUserExiste = true;
-      return;
-    }
 
+    //=================================================================================
     // TODO  Vérifier que la confirmation de mot de passe correspond au mot de passe
+    //=================================================================================
     if (this.form.form.invalid || this.model.password !== this.model.confirmPassword) {
-      this.passwordIsNotEquals = true;
+      this.nzMessageService.error("Les mots de passe ne concorde pas.");
       return;
-    } else {
-      this.userService.register(this.model.username, this.model.password);
     }
 
-    // TODO Enregistrer l'utilisateur via le UserService
-    this.goToLogin();
+    //===================================================================
+    // TODO Vérification que l'utilisateur n'est pas déjà existant.
+    //===================================================================
+    this.user.exists(this.model.username)
+      .then(reponse => {
+        if (reponse) {
+          this.nzMessageService.error("Cet utilisateur existe déjà.");
+          return;
+        } else {
+          try {
+            //===================================================================
+            // TODO Enregistrer l'utilisateur via le UserService
+            //===================================================================
+            this.userService.register(this.model.username, this.model.password)
+              .then(res => {
+                this.goToLogin();
+              })
+              .catch(err => {
+                this.nzMessageService.error("Une erreur est survenue. Veuillez réessayer plus tard");
+                console.log(err);
+                return;
+              });
+
+          } catch (e) {
+            console.log(e);
+            this.nzMessageService.error("Une erreur est survenue. Veuillez réessayer plus tard");
+          }
+        }
+      }).catch(err => {
+        console.log(err);
+        this.nzMessageService.error("Une erreur est survenue. Veuillez réessayer plus tard");
+        return;
+      })
   }
 
   goToLogin() {
