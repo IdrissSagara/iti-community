149b4b3647c8feea5445e776916efd09bc227241
Sagara Idriss <sagara.idriss@gmail.com>
2021-01-26 18:36:59 +0100

    first version to display notification

--- src/modules/notification/notification-bar/notification-bar.component.ts
+++ src/modules/notification/notification-bar/notification-bar.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {NotificationService} from '../services/notification.service';
-import {AnyNotification} from '../notification.model';
+import {AnyNotification, NewRoomNotification, PostLikedNotification} from '../notification.model';
+import {NotificationStore} from '../notification.store';
+import {NotificationQueries} from '../services/notification.queries';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-notification-bar',
@@ -8,16 +11,25 @@ import {AnyNotification} from '../notification.model';
   styleUrls: ['./notification-bar.component.less']
 })
 export class NotificationBarComponent implements OnInit {
+  anyNotifications: AnyNotification[];
 
-  anyNotif: AnyNotification;
-  notif: any;
-
-  constructor(private notifService: NotificationService) {
-  }
-
-  ngOnInit(): void {
+  constructor(private notifService: NotificationService, private notifStore: NotificationStore,
+              private notifQueries: NotificationQueries) {
 
   }
 
+  async ngOnInit() {
+    this.anyNotifications = await this.notifQueries.getNotifications();
+    console.log('any Notif', this.anyNotifications);
 
+  }
+
+  timeConversion(millisec: any) {
+    const date = new Date(millisec * 1000);
+    const hours = date.getHours();
+    const minutes = '0' + date.getMinutes();
+    const seconds = '0' + date.getSeconds();
+    const formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    return formattedTime;
+  }
 }
